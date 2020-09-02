486ab4ceceff92ba1c72219c07d72696a73baa72
asamaiya00 <asamaiya00@gmail.com>
2020-09-02 15:43:36 +0530

    get Logs

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, Fragment } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+import { SearchBar } from './components/layouts/SearchBar';
+import { Logs } from './components/logs/Logs';
+
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+
+export const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch('http://localhost:5000/logs');
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) return <h4>Loading...</h4>;
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4 className="center">System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center">No logs to show...</p>
+        ) : (
+          logs.map((log) => <LogItem log={log} key={log.id} />)
+        )}
+      </ul>
+    </div>
+  );
+};
